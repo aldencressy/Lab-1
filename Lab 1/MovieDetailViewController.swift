30f1cf56027486f6bed84c5ff0c6407f8139dc79
aldencressy <acressy@smu.edu>
2024-09-16 22:12:07 -0500

    Basic Table View

---
+++ Lab 1/MovieDetailViewController.swift
@@ -0,0 +1,8 @@
+//
+//  MovieDetailViewController.swift
+//  Lab 1
+//
+//  Created by Alden Cressy on 9/16/24.
+//
+
+import Foundation
---
+++ Lab 1/MovieTableViewController.swift
@@ -0,0 +1,8 @@
+//
+//  MovieTableViewController.swift
+//  Lab 1
+//
+//  Created by Alden Cressy on 9/16/24.
+//
+
+import Foundation
