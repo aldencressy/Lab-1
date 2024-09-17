b570e708e6039b31bd7c52b8e7caf2aab6a90803
aldencressy <acressy@smu.edu>
2024-09-16 20:32:46 -0500

    Initial Commit

---
+++ Lab 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab 1
+//
+//  Created by Alden Cressy on 9/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
