f8347a435616817088d59559fac77b76badb15dc
aldencressy <acressy@smu.edu>
2024-09-17 11:57:02 -0500

    feature - added movie images

--- Lab 1/MovieDetailViewController.swift
+++ Lab 1/MovieDetailViewController.swift
@@ -5,16 +5,29 @@ class MovieDetailViewController: UIViewController {
    
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var movieImageView: UIImageView!
     
     var movieTitle: String?
     var movieGenre: String?
+    var movieImageURL: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the labels with the passed data
         titleLabel.text = "Title: \(movieTitle ?? "Unknown")"
         genreLabel.text = "Genre: \(movieGenre ?? "Unknown")"
+        
+        if let imageUrlString = movieImageURL, let url = URL(string: imageUrlString) {
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let data = data, error == nil {
+                    DispatchQueue.main.async {
+                        // Set the image on the main thread
+                        self.movieImageView.image = UIImage(data: data)
+                    }
+                }
+            }.resume()
+        }
     }
+
 }
 
--- Lab 1/MovieTableViewController.swift
+++ Lab 1/MovieTableViewController.swift
@@ -3,6 +3,9 @@ import UIKit
 class MovieTableViewController: UITableViewController {
     let movies = ["Inception", "Interstellar", "The Matrix", "The Dark Knight", "Oppenheimer"]
     let genres = ["Sci-Fi", "Action", "Fiction", "SuperHero", "History"]
+    let movieImages = ["https://i.ebayimg.com/images/g/LTQAAOSw~gxfU1Rd/s-l1200.jpg", "https://i.etsystatic.com/23402008/r/il/b658b2/2327469308/il_570xN.2327469308_492n.jpg",
+    "https://m.media-amazon.com/images/I/71PfZFFz9yL._AC_UF894,1000_QL80_.jpg", "https://m.media-amazon.com/images/I/818hyvdVfvL._AC_UF894,1000_QL80_.jpg",
+    "https://m.media-amazon.com/images/I/719W5aAI5NL._AC_UF894,1000_QL80_.jpg"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,10 +28,12 @@ class MovieTableViewController: UITableViewController {
             if let indexPath = tableView.indexPathForSelectedRow {
                 let selectedMovie = movies[indexPath.row]
                 let selectedGenre = genres[indexPath.row]
+                let selectedImageURL = movieImages[indexPath.row]
                 let destinationVC = segue.destination as! MovieDetailViewController
                 
                 destinationVC.movieTitle = selectedMovie
                 destinationVC.movieGenre = selectedGenre
+                destinationVC.movieImageURL = selectedImageURL
             }
         }
     }
