af3f3101535220b333d5e91217786972e2c6debe
William Epperly <williamepperly@Williams-MBP.attlocal.net>
2024-09-22 14:33:36 -0500

    added timer/blinking logic

---
+++ Lab 1/WatchedMoviesMarker.h
@@ -0,0 +1,16 @@
+//
+//  WatchedMoviesMarker.h
+//  Lab 1
+//
+//  Created by William Epperly on 9/22/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WatchedMoviesMarker : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Lab 1/WatchedMoviesMarker.m
@@ -0,0 +1,12 @@
+//
+//  WatchedMoviesMarker.m
+//  Lab 1
+//
+//  Created by William Epperly on 9/22/24.
+//
+
+#import "WatchedMoviesMarker.h"
+
+@implementation WatchedMoviesMarker
+
+@end
