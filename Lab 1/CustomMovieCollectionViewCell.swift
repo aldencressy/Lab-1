af01656900781d206fcb77416b60731eecc48cc1
SamSkanse <sskanse@smu.edu>
2024-09-22 21:20:46 -0500

    Segmented Control

---
+++ Lab 1/CustomMovieCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomMovieCollectionViewCell.swift
+//  Lab 1
+//
+//  Created by Alden Cressy on 9/22/24.
+//
+
+import UIKit
+
+class CustomMovieCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
--- Lab 1/FavoritesCollectionViewController.swift
+++ Lab 1/FavoritesCollectionViewController.swift
@@ -1,63 +1,69 @@
-//
-//  FavoritesViewController.swift
-//  Lab 1
-//
-//  Created by Ashley Cardot on 9/21/24.
-//
-
 import UIKit
 
-class FavoritesCollectionViewController: UICollectionViewController {
+class FavoritesCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
-    var favoriteMovies = [Movie]()
+    var favoriteMovies = [Movie]() // Array of favorite movies
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Fetch favorite movies from the FavoritesManager
         favoriteMovies = FavoritesManager.shared.getFavorites()
         
-        print("Favorite Movies Count: \(favoriteMovies.count)")
+        print("Made it to custom view")
+        
+        // Reload the collection view to display the data
         collectionView.reloadData()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-            
-        // Update the list when returning from detail view
-        favoriteMovies = FavoritesManager.shared.getFavorites()
-        collectionView.reloadData()
+    // MARK: UICollectionViewDataSource
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
     }
     
-
-
-    // MARK: - Navigation
-
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return favoriteMovies.count
     }
-        
+
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FavoriteMovieCell", for: indexPath) as! MovieCollectionViewCell
-        let movie = favoriteMovies[indexPath.item]
-        //cell.textLabel?.text = movie.title
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FavoriteMovieCell", for: indexPath) as! CustomMovieCollectionViewCell
         
-        cell.configure(with: movie)
+        let movie = favoriteMovies[indexPath.row]
+        
+        // Set the movie title
+        cell.titleLabel.text = movie.title
+        
+        // Load the movie image asynchronously
+        if let url = URL(string: movie.imageURL) {
+            loadImage(from: url, into: cell.imageView)
+        }
         
         return cell
     }
     
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let selectedMovie = favoriteMovies[indexPath.row]
-        print("Selected movie: \(selectedMovie.title)")
-        performSegue(withIdentifier: "showMovieDetailFromFavorites", sender: selectedMovie)
+    // Function to load images asynchronously
+    private func loadImage(from url: URL, into imageView: UIImageView) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data, error == nil {
+                DispatchQueue.main.async {
+                    imageView.image = UIImage(data: data)
+                }
+            }
+        }.resume()
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showMovieDetailFromFavorites",
-            let destinationVC = segue.destination as? MovieDetailViewController,
-            let movie = sender as? Movie {
-            destinationVC.movie = movie
-        }
+    // Set size for each item (cell)
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let padding: CGFloat = 10  // Padding between cells
+        let numberOfItemsPerRow: CGFloat = 2 // Number of cells per row
+        let availableWidth = collectionView.frame.width - (padding * (numberOfItemsPerRow + 1)) // Total width minus padding
+        let widthPerItem = availableWidth / numberOfItemsPerRow // Width for each cell
+        
+        // Return size for each item, with width and height adjusted
+        let heightPerItem = widthPerItem * 1.5 + 30 // Assuming aspect ratio for image and height for label
+        return CGSize(width: widthPerItem, height: heightPerItem)
     }
-
+    
+    
 }
